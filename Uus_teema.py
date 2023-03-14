fe97f5c6418314dc1086331886697d5e2caa81b8
Loginov10 <120027474+Loginov10@users.noreply.github.com>
2023-03-14 14:39:37 +0200

    Update Uus_teema.py

--- Uus_teema.py
+++ Uus_teema.py
@@ -1,13 +1,22 @@
-﻿from tkinter import*  #Графическая оболочка
+from tkinter import*  #Графическая оболочка
 k=0
 def klikker():
     global k
     k+=1
     nupp.configure(text=k)
+    if k%2==0:
+        raam.itemconfig(pildid,image=pilt)
+    else:
+        raam.itemconfig(pildid,image=pilt2)
+
+def text_to_lbl(event):
+    text=tekst_kast.get()
+    pealkiri.configure(text=text)
+    tekst_kast.delete(0,END)
 
 tekst="Aken"
 aken=Tk()
-aken.geometry("500x700")  #разрешение экрана
+aken.geometry("800x900")  #разрешение экрана
 aken.title(tekst)          #заголовок
 aken.iconbitmap("mush.ico")       #иконка
 
@@ -28,9 +37,25 @@ nupp=Button(aken,
             activeforeground="red",
             height=3,    
             width=20,
-            command=klikker)
+            command=klikker)           
+raam=Canvas(aken,
+            width=481,
+            height=621,
+            bg="black",)
+pilt=PhotoImage(file="among.png")
+pilt2=PhotoImage(file="among2.png")
+pildid=raam.create_image(2,2,image=pilt,anchor=NW)    #растояние пикселей(отступ от начала),   anchor - якорь,начало картинки
+tekst_kast=Entry(aken,
+                 fg="lightblue",
+                 bg="grey",
+                 font="Dubai 20",
+                 width=20,
+                 justify=CENTER)
 
-pealkiri.pack()       #упаковка элементов
-nupp.pack()
+tekst_kast.bind("<Return>",text_to_lbl) #Enter
 
+pealkiri.pack()  #упаковка элементов
+tekst_kast.pack() #side=LEFT,RIGHT
+nupp.pack()           #pack отображает
+raam.pack()
 aken.mainloop()      
